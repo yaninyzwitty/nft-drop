99abe710bc4767c925248bf579f1baf73c4c50f3
yaninyzwitty <ianmwa143@gmail.com>
2022-11-14 22:11:14 +0300

    Creatin Login Page...metamask...

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
+
+
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThirdwebProvider desiredChainId={ChainId.Rinkeby}>
+
+    <Component {...pageProps} />
+    </ThirdwebProvider>
+  )
 }
